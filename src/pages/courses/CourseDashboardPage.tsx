a26de2f1b74f70c69bc71a537258ab3642d3c6f0
Matiassgg <matugrillo@hotmail.com>
2024-07-30 21:49:43 -0300

    #9 - Added validation on empty courses

--- src/pages/courses/CourseDashboardPage.tsx
+++ src/pages/courses/CourseDashboardPage.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { useCourseContext } from './contexts/CourseContext';
 import { Card, CardBody, CardText, CardTitle, Button } from 'reactstrap';
-import {useNavigate, useParams} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import SidebarCourses from './SidebarCourses';
 import './CourseDashboardPage.css';
 
@@ -12,35 +11,37 @@ export const CourseDashboardPage = () => {
   const handleViewCourse = (courseId: string) => {
     history(`/courses/${courseId}`);
   };
-  
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-          backgroundColor: '#f6effa',
-          width: '100vw',
-        }}
-      >
-        <SidebarCourses />
-      <div className="course-dashboard">
-      <div className="course-list">
-        {courses.map(course => (
-          <Card key={course.title} className="course-card">
-            <img src={course.imageUrl} alt={course.title} className="course-image" />
-            <CardBody>
-              <CardTitle tag="h5">{course.title}</CardTitle>
-              <CardText>{course.description}</CardText>
-              <Button color="primary" onClick={() => handleViewCourse(course.title)}>Ver Curso</Button>
-            </CardBody>
-          </Card>
-        ))}
-      </div>
-    </div>
-    </div>
 
-    
-    );
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundColor: '#f6effa',
+        width: '100vw',
+      }}
+    >
+      <SidebarCourses />
+      <ul className="course-dashboard">
+        <div className="course-list">
+          {!courses.length ? (
+            <h2>No hay cursos disponibles</h2>
+          ) : (
+            courses.map(course => (
+              <Card key={course.title} className="course-card">
+                <img src={course.imageUrl} alt={course.title} className="course-image" />
+                <CardBody>
+                  <CardTitle tag="h5">{course.title}</CardTitle>
+                  <CardText>{course.description}</CardText>
+                  <Button color="primary" onClick={() => handleViewCourse(course.title)}>Ver Curso</Button>
+                </CardBody>
+              </Card>
+            ))
+          )}
+        </div>
+      </ul>
+    </div>
+  );
 };

7e29de2ea90ecfad8928a32fd0aa8504bad23052
Matiassgg <matugrillo@hotmail.com>
2024-08-03 00:30:58 -0300

    #9 - Renamed and deleted files

--- src/components/NotImplementedPage.tsx
+++ src/components/NotImplementedPage.tsx
@@ -1,8 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 import { Card } from 'reactstrap';
+
 import logo_black_only from '../assets/images/logo_black_only.png';
 
-export default function NotImplemented() {
+export default function NotImplementedPage() {
 
   const navigate = useNavigate();
 

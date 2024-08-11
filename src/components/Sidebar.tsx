b213c59e265265752edaff654f9b5ed112b3a022
Matiassgg <matugrillo@hotmail.com>
2024-08-11 00:21:00 -0300

    #12 - Fix sidebar and adapted changes from main

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,13 +10,28 @@ import { reset } from "../redux/slices/user";
 import { RootState } from "../redux/store";
 import { del } from "../utils/network";
 
-const coursesManagementLinks = [
+const myCourses = [
   { to: '/courses/create', label: 'Crear curso' },
   { to: '/courses/dashboard', label: 'Ver Cursos' },
 ];
 
-export default function Sidebar() {
+const meStudent = [
+  { key: "1", to: '/me/learning-type', label: 'Test de aprendizaje' },
+  { key: "2", to: '/me/evaluations', label: 'Evaluaciones' },
+];
 
+const meProfile = [
+  { key: "1", to: '/me/profile', label: 'Mis datos' },
+  { key: "2", to: '/me/logout', label: 'Cerrar sesión' },
+];
+
+interface IProfile {
+  key: string
+  to: string;
+  label: string;
+}
+
+export default function Sidebar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((state: RootState) => state.user);
@@ -35,10 +50,10 @@ export default function Sidebar() {
       <img src={logo} alt="Proyecto Arima" className='sidebar-header' />
       <div className='sidebar-container'>
         <div className="w-100 d-flex flex-column gap-3">
-          {user.role === 'TEACHER' && (
+          {user?.role === 'TEACHER' && (
             <div className="w-100">
               <span className='sidebar-section-title'>Gestión de Cursos</span>
-              {coursesManagementLinks.map((link) => (
+              {myCourses.map((link) => (
                 <NavLink end to={link.to} key={link.to} className={({ isActive }) => isActive ? 'sidebar-navlink-active' : 'sidebar-navlink-inactive'}>
                   <NavItem className='sidebar-navlink-item'>
                     <FontAwesomeIcon icon={faCircle} style={{
@@ -52,38 +67,93 @@ export default function Sidebar() {
             </div>
           )}
 
-          <div className="w-100">
-            <span className='sidebar-section-title'>Mis Cursos</span>
-            {/* Mostrar los cursos en el sidebar */}
-            {courses?.map((course: ICourse) => (
-            <NavLink 
-              end 
-              to={`/courses/${course.id}`} 
-              key={course.id} 
-              className={({ isActive }) => isActive ? 'sidebar-navlink-active' : 'sidebar-navlink-inactive'}>
+          {user?.role && ['STUDENT', 'TEACHER'].includes(user.role) && (
+            <div className="w-100">
+              <span className='sidebar-section-title'>Mis Cursos</span>
+              {courses?.map((course: ICourse) => (
+                <NavLink
+                  end
+                  to={`/courses/${course.id}`}
+                  key={course.id}
+                  className={({ isActive }) => isActive ? 'sidebar-navlink-active' : 'sidebar-navlink-inactive'}>
+                  <NavItem className='sidebar-navlink-item'>
+                    <FontAwesomeIcon icon={faCircle} style={{
+                      width: '0.6rem',
+                      color: '#49454f',
+                    }} />
+                    <span>{course.title}</span>
+                  </NavItem>
+                </NavLink>
+              ))}
+            </div>
+          )}
+
+          {user?.role === 'STUDENT' && (
+            <div className="w-100">
+              <span className='sidebar-section-title'>Estudiante</span>
+              {meStudent.map((link) => (
+                <NavLink end
+                  to={link.to}
+                  key={link.key}
+                  className={({ isActive }) => isActive ? 'sidebar-navlink-active' : 'sidebar-navlink-inactive'}
+                >
+                  <NavItem className='sidebar-navlink-item'>
+                    <FontAwesomeIcon icon={faCircle} style={{
+                      width: '0.6rem',
+                      color: '#49454f',
+                    }} />
+                    <span>{link.label}</span>
+                  </NavItem>
+                </NavLink>
+              ))}
+            </div>
+          )}
+
+
+          {(user?.role === 'ADMIN') && (
+            <div className="w-100">
+              <span className='sidebar-section-title'>Administración</span>
+              <NavLink to={'/directors'} end className={({ isActive }) => isActive ? 'sidebar-navlink-active' : 'sidebar-navlink-inactive'}>
+                <NavItem className='sidebar-navlink-item'>
+                  <FontAwesomeIcon icon={faCircle} style={{
+                    width: '0.6rem',
+                    color: '#49454f',
+                  }} />
+                  <span>Directivos</span>
+                </NavItem>
+              </NavLink>
+              <NavLink to={'/directors/new'} end className={({ isActive }) => isActive ? 'sidebar-navlink-active' : 'sidebar-navlink-inactive'}>
+                <NavItem className='sidebar-navlink-item'>
+                  <FontAwesomeIcon icon={faCircle} style={{
+                    width: '0.6rem',
+                    color: '#49454f',
+                  }} />
+                  <span>Crear Directivo</span>
+                </NavItem>
+              </NavLink>
+            </div>
+          )}
+
+        </div>
+
+        <div className="w-100">
+          <span className='sidebar-section-title'>Perfil</span>
+          {meProfile?.map((profile: IProfile) => (
+            <NavLink
+              to={profile.to} end
+              key={profile.key}
+              className={({ isActive }) => isActive ? 'sidebar-navlink-active' : 'sidebar-navlink-inactive'}
+              onClick={profile.key === '2' ? signout : undefined}
+            >
               <NavItem className='sidebar-navlink-item'>
                 <FontAwesomeIcon icon={faCircle} style={{
                   width: '0.6rem',
                   color: '#49454f',
                 }} />
-                <span>{course.title}</span>
+                <span>{profile.label}</span>
               </NavItem>
             </NavLink>
           ))}
-          </div>
-        </div>
-
-        <div className="w-100">
-          <span className='sidebar-section-title'>Opciones</span>
-          <NavLink to={'/login'} end className={({ isActive }) => isActive ? 'sidebar-navlink-active' : 'sidebar-navlink-inactive'}>
-            <NavItem className='sidebar-navlink-item' onClick={signout}>
-              <FontAwesomeIcon icon={faCircle} style={{
-                width: '0.6rem',
-                color: '#49454f',
-              }} />
-              <span>Cerrar sesión</span>
-            </NavItem>
-          </NavLink>
         </div>
 
       </div>

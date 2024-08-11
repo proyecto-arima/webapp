1256900091bd4ff1f2de4cc64ec4e9def6979750
Matiassgg <matugrillo@hotmail.com>
2024-08-11 03:08:34 -0300

    #12 - Mayor refactor on learning type form - Added validations on finish and in progress - Added result view on finish - Refactor messages based on figma view - Minor changes on css styles

--- src/pages/user-profile/StudentProfileLearningTypePage.tsx
+++ src/pages/user-profile/StudentProfileLearningTypePage.tsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { get } from '../../utils/network';
 
-import '../../assets/styles/profile-page.css'
+import '../../assets/styles/LearningTypePage.css'
 
 interface IOption {
   text: string;
@@ -16,19 +16,31 @@ interface IQuestion {
   options: IOption[];
 }
 
-export const StudentProfileLearningTypePage = () => {
-  const [testInProgress, setTestInProgress] = useState<boolean>(false);
-  const [testCompleted, setTestCompleted] = useState<boolean>(false);
-  const [testHasMoreQuestions, setTestHasMoreQuestions] = useState<boolean>(true);
-  const [learningType, setLearningType] = useState<string>('');
-  const [questionsMessageStatus, setQuestionsMessageStatus] = useState<string>('');
-  const [showTestMessage, setShowTestMessage] = useState<boolean>(false);
+const testPreviousComments = [
+  "Ubicate en un lugar tranquilo con conexión a internet",
+  "Podes poner música de fondo",
+  "Te llevará entre X y X minutos realizar todo el text",
+  "Al finalizar el resultado aparecerá en pantalla y quedará asociado a tu usuario",
+  "Es importante que lo respondas con sinceridad ya que el contenido de la plataforma se adaptará en función del resultado  obtenido"
+];
 
+const testPostComments = [
+  "El test de kolb fue desarrollado en 1984",
+  "El test te permite descubrir que forma de aprender es la que mejor se adapta a vos",
+  "Podes repetir el test cuantas veces quieras!"
+];
+
+export const StudentProfileLearningTypePage = () => {
+  const [isTestInProgress, setTestInProgress] = useState<boolean>(false);
+  const [isTestCompleted, setTestCompleted] = useState<boolean>(false);
+  const [testHasMoreQuestions, setTestHasMoreQuestions] = useState<boolean>(true);
+
+  const [showAlertMessage, setShowAlertMessage] = useState<boolean>(false);
+  const [alertMessage, setAlertMessage] = useState<string>('');
+
+  const [learningType, setLearningType] = useState<string>('');
   const user = useSelector((state: RootState) => state.user);
   
-  // TODO: Mocked user learning profile. Get from redux
-  const learningProfile = "VISUALIZADOR";
-
   // TODO: Questions mocks
   const [questions, setQuestions] = useState<IQuestion[]>([
     {
@@ -118,15 +130,15 @@ export const StudentProfileLearningTypePage = () => {
   ]);
 
   useEffect(() => {
-    if (!learningProfile) {
-      // fetch(`${API_URL}/students/${user.id}/learning-profile`, {
-      //   method: 'POST',
-      //   headers: { 'Content-Type': 'application/json' }
-      // }).then(res => res.json())
-      //   .then(res => res.data)
-      //   .then(data => { setLearningType(data.learningProfile) });
+      // TODO: Mocked user learning profile. Get from redux
+    setLearningType("VISUALIZADOR");
+    return;
+
+    // TODO: Fetch learning profile from backend
+    if (!learningType) {
       get(`/students/${user.id}/learning-profile`)
         .then(res => { return res.ok ? res.json() : Promise.reject(res) })
+        // TODO: Set learningProfile attribute
         .then(res => { return res.data ? setLearningType(res.data.learningProfile) : Promise.reject(res) })
         .catch(err => console.error(`An error ocurred while fetching learning profile: ${err}`));
     }
@@ -134,108 +146,124 @@ export const StudentProfileLearningTypePage = () => {
 
   const doTest = () => {
     setTestInProgress(true);
+    setTestHasMoreQuestions(true);
+  }
+
+  function allQuestionsAnswered(questionsLeft: IQuestion[]) {
+    const areQuestionsAnswered = questionsLeft.every(question =>
+      question.options.every(option => option.valueAsigned !== '')
+    );
+    if (!areQuestionsAnswered) {
+      setAlertMessage('Termina de responder estas preguntas antes de continuar');
+      setShowAlertMessage(true);
+      window.setTimeout(() => {
+        setShowAlertMessage(false);
+      }, 3000);
+    }
+    return areQuestionsAnswered;
   }
 
   const finishTest = () => {
-    if(testCompleted) {
-      setQuestionsMessageStatus('Test completado con éxito!');
+    setTestCompleted(allQuestionsAnswered(questions));
+    if (isTestCompleted) {
+      setTestCompleted(true);
+      setAlertMessage('Test completado con éxito!');
       window.setTimeout(() => {
         setTestInProgress(false);
-        setQuestionsMessageStatus('');
-        setShowTestMessage(false);
-        setTestCompleted(false);
+        setShowAlertMessage(false);
       }, 3000);
       return;
     } else {
-      setShowTestMessage(true);
-      setQuestionsMessageStatus('Debes completar todas las preguntas');
+      setAlertMessage('El test no esta completado, por favor responde todas las preguntas');
+      setShowAlertMessage(true);
       window.setTimeout(() => {
-        setQuestionsMessageStatus('');
+        setShowAlertMessage(false);
       }, 3000);
+      return;
     }
   }
 
   const getQuestions = (questionsLeft: IQuestion[]) => {
-  const sliceQuestions = () => {
-    setQuestions(questions.slice(3));
-  }
-
-    const hasNextQuestions = (newQuestions: IQuestion[]) : boolean => {
+    const nextQuestionsForm = (newQuestions: IQuestion[]): boolean => {
       const hasMoreQuestions = newQuestions.slice(3).length > 0;
       setTestHasMoreQuestions(hasMoreQuestions);
+      if (hasMoreQuestions) {
+        setQuestions(questions.slice(3));
+      }
       return hasMoreQuestions;
     }
-  
-    // TODO: Quiza ni es necesario
-    const noMoreQuestions = () => {
-      setShowTestMessage(true);
-      setQuestionsMessageStatus('Estas son las ultimas preguntas');
-      window.setTimeout(() => {
-        setQuestionsMessageStatus('');
-      }, 3000);
-    }
+
+
 
     return (
       <div>
-        {showTestMessage && <p className={testCompleted ? "text-success" : "text-danger"}>{questionsMessageStatus}</p>}
-        {!questionsLeft  && <p className={testHasMoreQuestions && !testCompleted ? "text-success" : "text-danger"}>No hay mas preguntas</p>}
-        <h2>Test de Kolb</h2>
-        <p>Se puntúa con 1 la que menos te representa y con 4 la que más te representa</p>
-        <p>Las 4 deben quedar con puntuación y no se pueden repetir valores.</p>
-        <ul>
-          {questionsLeft.slice(0,3).map((question, indexQuestions) => (
-            <div key={indexQuestions}>
-              <h3>{question.statement}</h3>
+        {showAlertMessage && <p className={isTestCompleted ? "text-success" : "text-danger"}>{alertMessage}</p>}
+        <div>
+          <h2>Test de aprendizaje</h2>
+          <Card className='profile-card-questions'>
+            <CardBody>
               <ul>
-                {question.options.map((option, indexOptions) => (
-                  <div key={indexOptions}>
-                    <label>{option.text}</label>
-                    <select
-                      className='test-select'
-                      onChange={(e) => {
-                        questions[indexQuestions].options[indexOptions].valueAsigned = e.target.value;
-                        
-                        {/* DEBUG: Testing finish test */}
-                        setTestCompleted(true);
-                      }}
-                      value={option.valueAsigned}
-                    >
-                      <option id={`question-${indexQuestions}-${indexOptions}`} value="1">1</option>
-                      <option id={`question-${indexQuestions}-${indexOptions}`} value="2">2</option>
-                      <option id={`question-${indexQuestions}-${indexOptions}`} value="3">3</option>
-                      <option id={`question-${indexQuestions}-${indexOptions}`} value="4">4</option>
-                    </select>
+                {questionsLeft.slice(0, 3).map((question, indexQuestions) => (
+                  <div key={indexQuestions}>
+                    <h3>{question.statement}</h3>
+                    <ul>
+                      {question.options.map((option, indexOptions) => (
+                        <div key={indexOptions}>
+                          <label>{option.text}</label>
+                          <select
+                            className='profile-learningtype-actions'
+                            onChange={(e) => {
+
+                              questions[indexQuestions].options[indexOptions].valueAsigned = e.target.value;
+                            }}
+                            // value={option.valueAsigned}
+                            defaultValue={option.valueAsigned}
+                          >
+                            <option id={`question-${indexQuestions}-${indexOptions}`} value="1">1</option>
+                            <option id={`question-${indexQuestions}-${indexOptions}`} value="2">2</option>
+                            <option id={`question-${indexQuestions}-${indexOptions}`} value="3">3</option>
+                            <option id={`question-${indexQuestions}-${indexOptions}`} value="4">4</option>
+                          </select>
+                        </div>
+                      ))}
+                    </ul>
                   </div>
                 ))}
               </ul>
-            </div>
-          ))}
-        </ul>
-        <ul>
-          <button className="btn-purple-primary" onClick={() => finishTest()}>
-            Finalizar test
-          </button>
-        </ul>
 
-        <ul>
-          <button className="btn-purple-primary" onClick={() => history.go(0)}>
-            Volver al inicio
-          </button>
+              <ul>
+                {isTestInProgress && questionsLeft.length <= 3 &&
+                  <div>
+                    {/* BUG: Deberia volver a las pregunta anterior */}
+                    <button className="btn-purple-primary" onClick={() => history.back()}>
+                      Atras
+                    </button>
 
-          <button className="btn-purple-primary" onClick={() => {
-            if (hasNextQuestions(questions)) {
-              sliceQuestions();
-              // DEBUG
-              console.log("Siguiente");
-            } else {
-              noMoreQuestions();
-            }
-          }}>
-            Siguiente
-          </button>
-        </ul>
+                    <button className="btn-purple-primary" onClick={() => finishTest()}>
+                      Finalizar test
+                    </button>
+                  </div>
+                }
+              </ul>
+
+              <ul>
+                {/* BUG: Deberia volver a la pantalla de aprendizaje */}
+                <button className="btn-purple-primary" onClick={() => history.back()}>
+                  Volver al inicio
+                </button>
+
+                {testHasMoreQuestions && questionsLeft.length > 3 &&
+                  <button className='btn-purple-primary' onClick={() => nextQuestionsForm(questionsLeft) && getQuestions(questionsLeft)}>
+                    Siguiente
+                  </button>
+                }
+              </ul>
+            </CardBody>
+          </Card>
+        </div>
       </div>
     );
+
   }
 
   return (
@@ -249,61 +277,69 @@ export const StudentProfileLearningTypePage = () => {
         width: '100vw',
       }}
     >
-      {!testInProgress ?
+      {!isTestInProgress ?
         <div className="container">
+
+          <div className='profile-learningtype-page-container'>
+            <h1>Test de aprendizaje</h1>
+          </div>
+
           <div className="row">
             <ul>
-              <h2>{learningType ? `Tu tipo de aprendizaje actualmente es ${learningType}` :
-                "Comenza el test para descubrir tu tipo de aprendizaje, son menos de 5 minutos!"
-              }</h2>
-            </ul>
-            <ul>
-              <Card className='profile-card'>
-                <CardTitle tag="h5">Consideraciones del test</CardTitle>
+              <Card className='profile-card-init'>
+                <CardTitle tag="h5">Consideraciones antes de realizar el test</CardTitle>
                 <CardBody>
-                  <div>
-                    <Input type='checkbox' checked={true} />
-                    <span>El test es una herramienta de autoconocimiento y no una verdad absoluta.</span>
-                  </div>
-                  <div>
-                    <Input type='checkbox' checked={true} />
-                    <span>El test no refleja la totalidad de tu personalidad o tu forma de estudiar</span>
-                  </div>
-                  <div>
-                    <Input type='checkbox' checked={true} />
-                    <span>El test esta pensado para adaptarte el contenido de la plataforma en base a tus preferencias</span>
-                  </div>
+                  {testPreviousComments.map((comment, index) => (
+                    <div key={index}>
+                      <Input type='checkbox' defaultChecked />
+                      <span>{comment}</span>
+                    </div>
+                  ))}
                 </CardBody>
               </Card>
 
             </ul>
-            <ul>
-              <Card className='profile-card'>
-                <CardTitle tag="h5">Queres saber mas?</CardTitle>
-                <CardBody>
-                  <div>
-                    <Input type='checkbox' checked={true} />
-                    <span>El test de kolb fue desarrollado en 1984</span>
-                  </div>
-                  <div>
-                    <Input type='checkbox' checked={true} />
-                    <span>El test te permite descubrir que forma de aprender es la que mejor se adapta a vos</span>
-                  </div>
-                  <div>
-                    <Input type='checkbox' checked={true} />
-                    <span>Podes repetir el test cuantas veces quieras!</span>
-                  </div>
-                </CardBody>
-              </Card>
-            </ul>
-
             <ul>
               <button className="btn-purple-primary" onClick={() => doTest()}>
                 Comenzar test
               </button>
             </ul>
           </div>
-        </div> : getQuestions(questions)
+        </div> : isTestCompleted ?
+          <div>
+            <ul>
+              <Card className='profile-card-init'>
+                <CardTitle tag="h1">Tu resultado</CardTitle>
+                <CardBody>
+                  Tu estilo de aprendizaje <strong>{learningType}</strong> implica que asimilas mejor la información a través de la escucha.
+                  Probablemente prefieras recibir información de manera verbal y encuentres más efectivo escuchar explicaciones,
+                  participar en discusiones y debates para comprender el contenido. Tienes una gran capacidad para recordar diálogos y sonidos,
+                  y a menudo recurres a la repetición en voz alta y al diálogo interno como métodos de estudio.
+
+                  Además, eres especialmente sensible a los matices y tonos de voz, lo que te permite captar detalles adicionales en
+                  la comunicación.
+
+                  Es probable que disfrutes participar activamente en discusiones grupales y actividades auditivas,
+                  ya que estas te facilitan la consolidación de tu aprendizaje.
+                </CardBody>
+              </Card>
+            </ul>
+
+            <ul>
+              <Card className='profile-card-init'>
+                <CardTitle tag="h5">Queres saber mas?</CardTitle>
+                <CardBody>
+                  {testPostComments.map((comment, index) => (
+                    <div key={index}>
+                      <Input type='checkbox' defaultChecked/>
+                      <span>{comment}</span>
+                    </div>
+                  ))}
+                </CardBody>
+              </Card>
+            </ul>
+
+          </div> : isTestCompleted ? <h3>Test finalizado!</h3> : getQuestions(questions)
       }
     </div>
   );

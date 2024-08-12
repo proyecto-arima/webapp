b39e61443d56341062d07dd52f5641cd99aaccfe
Matiassgg <matugrillo@hotmail.com>
2024-08-11 23:09:44 -0300

    #12 - Student learning type done, pending minor bugs

--- src/routes/ProfileRouter.tsx
+++ src/routes/ProfileRouter.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { UserProfilePage } from '../pages/user-profile/UserProfilePage';
-import { StudentProfileLearningTypePage } from '../pages/user-profile/StudentProfileLearningTypePage';
+import { StudentLearningTypePage } from '../pages/user-profile/StudentLearningTypePage';
+import { StudentLearningTypeForm } from '../pages/user-profile/StudentLearningTypeForm';
+import { StudentLearningTypeResult } from '../pages/user-profile/StudentLearningTypeResult';
 
 const ProfileRouter: React.FC = () => {
   return (
       <Routes>
         <Route path="profile" element={<UserProfilePage />} />
-        <Route path="learning-type" element={<StudentProfileLearningTypePage />} />
-        <Route path="evaluations" element={<StudentProfileLearningTypePage />} />
+        <Route path="learning-type" element={<StudentLearningTypePage />} />
+        <Route path="learning-type/test" element={<StudentLearningTypeForm />} />
+        <Route path="learning-type/result" element={<StudentLearningTypeResult />} />
+        <Route path="evaluations" element={<StudentLearningTypePage />} />
       </Routes>
   );
 };

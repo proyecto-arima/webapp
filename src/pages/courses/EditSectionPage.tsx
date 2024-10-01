d186dd5d2e1fe6a33d87de87ac7b49313c53c8c2
Ignacio Oscar García <ignaciogarcia@frba.utn.edu.ar>
2024-10-01 17:08:04 -0300

    #68 - Mejore estilos en editar seccion

--- src/pages/courses/EditSectionPage.tsx
+++ src/pages/courses/EditSectionPage.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { get, patch } from '../../utils/network';
-import { Card, Input } from 'reactstrap';
-import '../../assets/styles/CourseDetailPage.css';
+import { get, patch, post } from '../../utils/network';
+import { Card, Input, Label } from 'reactstrap';
+import Swal from 'sweetalert2';
 import { ConfirmDialog } from '../../components/ConfirmDialog';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons';
 
 interface ISection {
   id: string;
   name: string;
   description: string;
   visible: boolean;
+  image?: string;
 }
 
 export const EditSectionPage: React.FC = () => {
@@ -19,30 +22,33 @@ export const EditSectionPage: React.FC = () => {
     name: '',
     description: '',
     visible: true,
+    image: ''
   });
   const [editConfirmOpen, setEditConfirmOpen] = useState(false); // Estado para el diálogo
   const [loading, setLoading] = useState(true); // Estado de carga
+  const [autoGenerateImage, setAutoGenerateImage] = useState<boolean>(true);
+  const [generatedImage, setGeneratedImage] = useState<string | null>(null);
+  const [imageLoading, setImageLoading] = useState<boolean>(false);
   const navigate = useNavigate();
 
   // Fetch section data when the page loads
   useEffect(() => {
     if (sectionId) {
-      console.log('Fetching section data...'); // Para verificar que se haga la llamada
       get(`/courses/${courseId}/sections/${sectionId}`)
         .then(res => res.json())
         .then(data => {
-          console.log('Datos recibidos:', data); // Verifica los datos recibidos
           setFormData({
             id: data.id,
             name: data.name,
-            description: data.description || '', // Evitar que sea undefined
+            description: data.description || '',
             visible: data.visible,
+            image: data.image || ''
           });
-          setLoading(false); // Desactivar el estado de carga cuando los datos estén listos
+          setLoading(false);
         })
         .catch(err => {
           console.error('Error fetching section data:', err);
-          setLoading(false); // Asegurar que el estado de carga se desactive en caso de error
+          setLoading(false);
         });
     }
   }, [courseId, sectionId]);
@@ -62,14 +68,38 @@ export const EditSectionPage: React.FC = () => {
 
   const confirmEdit = async () => {
     if (sectionId) {
-      await patch(`/courses/${courseId}/sections/${sectionId}`, formData);
-      navigate(`/courses/${courseId}`); // Redirigir de nuevo a la página de detalles del curso
+      const updatedData = {
+        ...formData,
+        ...(autoGenerateImage ? { image: generatedImage } : {})
+      };
+      await patch(`/courses/${courseId}/sections/${sectionId}`, updatedData);
+      navigate(`/courses/${courseId}`);
     }
   };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    toggleEditConfirm(); // Mostrar el diálogo de confirmación
+    toggleEditConfirm();
+  };
+
+  const generateImage = () => {
+    if (!formData.name || !formData.description) {
+      return Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Debes ingresar el nombre y la descripción de la sección antes de generar la imagen',
+      });
+    }
+
+    setImageLoading(true);
+
+    post('/images', {
+      name: formData.name,
+      description: formData.description,
+    }).then((res) => res.json()).then((res) => {
+      setImageLoading(false);
+      setGeneratedImage(res.data);
+    });
   };
 
   if (loading) {
@@ -81,15 +111,34 @@ export const EditSectionPage: React.FC = () => {
       style={{
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'flex-start',
         height: '100vh',
         backgroundColor: '#f6effa',
         width: '100vw',
       }}
     >
-      <Card style={{ width: '30rem', paddingInline: '2rem', paddingBlock: '1rem' }}>
-        <h2 className="text-center mb-3">Editar Sección</h2>
-        <form onSubmit={handleSubmit}>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        padding: '20px',
+        width: '100%',
+        height: '100%',
+      }}>
+        <Card style={{ paddingInline: '2rem', paddingBlock: '1rem', width: '100%', height: '100%' }}>
+          <h1>Editar Sección</h1>
+          <hr />
+          <p style={{
+            textAlign: 'left',
+            marginBottom: '2rem',
+            color: '#6b7280'
+          }}>
+            Aquí podrás editar la sección en la plataforma <br />
+            Cada sección tendrá diferentes contenidos agrupados por temas, franjas de tiempo, etc.  
+            Opcionalmente podrás cargar una imagen de portada, sino, nosotros la crearemos por tí.<br />
+          </p>
+          <h3>Detalles de la sección</h3>
+          <hr />
           <Input
             type="text"
             name="name"
@@ -107,22 +156,95 @@ export const EditSectionPage: React.FC = () => {
             className="mb-3"
             onChange={handleChange}
           />
-          <div className="mb-3">
-            <label>
-              <input
-                type="checkbox"
-                name="visible"
-                checked={formData.visible}
-                onChange={handleChange}
-              />
-              Visible
-            </label>
+
+          <div className='d-flex flex-row mb-3 gap-3'>
+            <Input
+              type="checkbox"
+              name="visible"
+              id="visible"
+              checked={formData.visible}
+              onChange={handleChange}
+            />
+            <Label for="visible">Visible</Label>
           </div>
-          <button type="submit" className="btn-purple-1 w-100">
-            Actualizar
-          </button>
-        </form>
-      </Card>
+
+          <h3>Imagen</h3>
+          <hr />
+          <p style={{
+            textAlign: 'left',
+            marginBottom: '2rem',
+            color: '#6b7280'
+          }}>
+            Para generar una imagen automáticamente a partir del nombre y descripción de la sección, clickea en Generar Imagen y espera que la magia ocurra.<br />
+            También puedes utilizar una URL para elegir manualmente la imagen de la sección.
+          </p>
+
+          <div className='d-flex flex-row mb-3 gap-3'>
+            <Input type='checkbox' name='auto-generate' id='auto-generate' onClick={() => setAutoGenerateImage(!autoGenerateImage)} checked={autoGenerateImage} />
+            <Label for='auto-generate'>Generar imagen automáticamente</Label>
+          </div>
+
+          {autoGenerateImage ? (
+            <div style={{
+              flex: '1',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+              {imageLoading ? (
+                <button style={{
+                  backgroundColor: '#4d3a8e',
+                  color: 'white',
+                  padding: '1rem',
+                  borderRadius: '5px',
+                  cursor: 'pointer',
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  gap: '1rem',
+                }} disabled>
+                  <FontAwesomeIcon icon={faWandMagicSparkles} spin />
+                  Generando Imagen...
+                </button>
+              ) : (
+                generatedImage ? (
+                  <img src={generatedImage} alt="Generated" style={{
+                    width: '200px',
+                    borderRadius: '0.5rem',
+                    objectFit: 'cover',
+                    overflow: 'hidden',
+                  }} />
+                ) : (
+                  <button style={{
+                    backgroundColor: '#4d3a8e',
+                    color: 'white',
+                    padding: '1rem',
+                    borderRadius: '5px',
+                    cursor: 'pointer',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: '1rem',
+                  }} onClick={generateImage}>
+                    <FontAwesomeIcon icon={faWandMagicSparkles} />
+                    Generar Imagen
+                  </button>
+                )
+              )}
+            </div>
+          ) : (
+            <div style={{ flex: '1' }}>
+              <Input name="image" type="text" placeholder="URL de la imagen" className="mb-3" onChange={handleChange} />
+            </div>
+          )}
+
+          <div className='d-flex flex-row justify-content-end'>
+            <button className="btn-purple-1" onClick={handleSubmit}>
+              Actualizar
+            </button>
+          </div>
+        </Card>
+      </div>
 
       {/* Confirmación para editar la sección */}
       <ConfirmDialog

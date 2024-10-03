3560d44acb25579cf8b028295ac834449818a64d
Ignacio Oscar García <ignaciogarcia@frba.utn.edu.ar>
2024-10-03 20:23:23 -0300

    #71 - Editar contenido + pop ups funcionando

--- src/pages/sections/SectionContentDashboardForm/SectionContentDashboardFormTeachers.tsx
+++ src/pages/sections/SectionContentDashboardForm/SectionContentDashboardFormTeachers.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { Table } from "reactstrap";
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import empty from '../../../assets/images/empty.svg';
 import { IContent } from "../SectionContentDashboardPage";
@@ -12,54 +14,58 @@ interface ISectionContentDashboardFormTeachersProps {
   sectionId: string;
 }
 
-export default function SectionContentDashboardFormTeachers({ content, user, courseId, sectionId}: ISectionContentDashboardFormTeachersProps) {
-
+export default function SectionContentDashboardFormTeachers({ content, user, courseId, sectionId }: ISectionContentDashboardFormTeachersProps) {
   const navigate = useNavigate();
 
-  return (content?.length ? <Table>
-    <thead>
-      <tr>
-        <th>Contenido</th>
-        <th>Modo de publicación</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      {content.map(c => (
-        <tr key={c.id}>
-          <td>{c.title}</td>
-          <td>
-            {
-              c.publicationType === 'AUTOMATIC' ? 'Automático' :
-              c.publicationType === 'DEFERRED' ? 'Diferido' : 'Default'
-            }
-          </td>
-          <td style={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            gap: '1rem',
-          }}>
-            <button className="btn-purple-2"
-              onClick={() => {
-                navigate(`/courses/${courseId}/sections/${sectionId}/content/${c.id}?url=${encodeURIComponent(c.presignedUrl)}`);
-              }}
-            >Ver PDF</button>
-            <button className="btn-purple-1"
-              onClick={() => {
-                navigate(`/courses/${courseId}/sections/${sectionId}/content/${c.id}/map`);
-              }}
-            >Ver contenido generado</button>
-          </td>
+  return (content?.length ? (
+    <Table>
+      <thead>
+        <tr>
+          <th>Contenido</th>
+          <th>Modo de publicación</th>
+          <th></th>
         </tr>
-      ))}
-    </tbody>
-  </Table> : <div className='d-flex flex-column justify-content-center align-items-center h-100'>
-    <img
-      src={empty}
-    ></img>
-    <>
+      </thead>
+      <tbody>
+        {content.map(c => (
+          <tr key={c.id}>
+            <td>{c.title}</td>
+            <td>
+              {c.publicationType === 'AUTOMATIC' ? 'Automático' :
+                c.publicationType === 'DEFERRED' ? 'Diferido' : 'Default'
+              }
+            </td>
+            <td style={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              gap: '1rem',
+            }}>
+              <button className='btn-purple-2' onClick={() => {
+                  navigate(`/courses/${courseId}/sections/${sectionId}/contents/${c.id}/edit-title`);
+                }}>
+              <FontAwesomeIcon icon={faEdit} />
+
+              </button>
+              <button className="btn-purple-2"
+                onClick={() => {
+                  navigate(`/courses/${courseId}/sections/${sectionId}/content/${c.id}?url=${encodeURIComponent(c.presignedUrl)}`);
+                }}
+              >Ver PDF</button>
+              <button className="btn-purple-1"
+                onClick={() => {
+                  navigate(`/courses/${courseId}/sections/${sectionId}/content/${c.id}/map`);
+                }}
+              >Ver contenido generado</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  ) : (
+    <div className='d-flex flex-column justify-content-center align-items-center h-100'>
+      <img src={empty}></img>
       <h3>Parece que aún no has subido ningún contenido en esta sección.</h3>
       <h4>Por favor sube un contenido para que pueda ser procesado por AdaptarIA</h4>
-    </>
-  </div>)
+    </div>
+  ));
 }

7879ae5f2b443ced1db487fd654e1cbea4e299a3
Matiassgg <matugrillo@hotmail.com>
2024-10-17 18:52:22 -0300

    #44 - Added buttons of google login and fixs on styles

--- src/components/RecoverPasswordForm.tsx
+++ src/components/RecoverPasswordForm.tsx
@@ -13,28 +13,41 @@ interface IResetPasswordFormProps {
 export default function RecoverPasswordForm({ recoverPassword: recoverPassword, statusSended, statusMessage }: IResetPasswordFormProps) {
   const [email, setEmail] = useState('');
   const navigate = useNavigate();
-  
+
   return (
     <div>
       <div className="mb-3">
-        <Label htmlFor="email" className="form-label">Ingresa la dirección de correo asociada a la plataforma</Label>
+        <Label htmlFor="email" className="form-label">Ingresa la dirección de correo que tenes asociada en la plataforma</Label>
         <Input type="email" id="email" placeholder="Ingresá tu correo registrado" onChange={(e) => setEmail(e.target.value)} />
-        {statusMessage && <p className={statusSended ? "text-success" : "text-danger"}>{statusMessage}</p>}
+        {statusMessage &&
+          <p
+            style={{
+              display: "flex",
+              color: statusSended ? "green" : "red",
+              marginTop: "1rem",
+              marginBottom: "1rem",
+            }}
+          >{statusMessage}</p>
+        }
       </div>
-      <p className="text-muted">Te enviaremos a tu correo electrónico un enlace que te permitirá restablecer tu contraseña</p>
+
+      {!statusMessage &&
+          <p
+            style={{
+              display: "flex",
+              backgroundColor: "transparent",
+              border: "none",
+              color: "#6c757d",
+              textDecoration: "none",
+              marginTop: "1rem",
+              marginBottom: "1rem",
+            }}
+          >Te enviaremos a tu correo electrónico un enlace que te permitirá reestablecer tu contraseña</p>
+        }
+        
       <div className="d-flex justify-content-between mt-4">
-        <button onClick={() => navigate('/login')} style={{
-          backgroundColor: "transparent",
-          border: "none",
-          color: "#6c757d",
-          textDecoration: "underline",
-          cursor: "pointer",
-        }}>
-          Volver
-        </button>
-        <button className="btn-purple-1" onClick={() => email && recoverPassword(email)}>
-          Recuperar contraseña
-        </button>
+        <button onClick={() => navigate('/login')} className="btn-purple-2">Volver</button>
+        <button className="btn-purple-1" onClick={() => email && recoverPassword(email)}>Recuperar contraseña</button>
       </div>
     </div>
   );

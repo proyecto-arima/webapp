9df792ec37de5d91492dcc3a77a3723989f26e0e
Matiassgg <matugrillo@hotmail.com>
2024-10-25 21:06:52 -0300

    #107 - Changed files location & router

--- src/routes/ProfileRouter.tsx
+++ src/routes/ProfileRouter.tsx
@@ -6,7 +6,6 @@ import { StudentLearningTypePage } from '../pages/user-profile/StudentLearningTy
 import { StudentLearningTypeForm } from '../pages/user-profile/StudentLearningTypeForm';
 import { StudentLearningTypeResult } from '../pages/user-profile/StudentLearningTypeResult';
 import { SurveyPage } from '../pages/SurveyPage';
-import { UserCreationMassivePage } from '../pages/teachers/UserCreationMassivePage';
 
 const ProfileRouter: React.FC = () => {
   return (
@@ -16,7 +15,6 @@ const ProfileRouter: React.FC = () => {
         <Route path="learning-type" element={<StudentLearningTypePage />} />
         <Route path="learning-type/test" element={<StudentLearningTypeForm />} />
         <Route path="learning-type/result" element={<StudentLearningTypeResult />} />
-        <Route path="/users/bulk" element={<UserCreationMassivePage />} />
       </Routes>
   );
 };

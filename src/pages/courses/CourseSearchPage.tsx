3fc57cfcc43fa8f1fe9970281d2fa8a72091b1e0
Ignacio Oscar García <ignaciogarcia@frba.utn.edu.ar>
2024-10-30 21:03:35 -0300

    #137 - Texto de codigo de matriculacion coincide con HU

--- src/pages/courses/CourseSearchPage.tsx
+++ src/pages/courses/CourseSearchPage.tsx
@@ -51,7 +51,7 @@ export const CourseSearchPage = () => {
 
   const handleEnroll = async (courseId: string) => {
     const { value: matriculationCode } = await Swal.fire({
-      title: 'Ingresá el código de matriculación provisto por el docente',
+      title: 'Ingrese el código de matriculación provisto por el docente',
       input: 'text',
       inputLabel: 'Código de matriculación',
       confirmButtonText: 'Confirmar matriculación',

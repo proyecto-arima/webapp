babc485543d0262ea8ffd9e5a76d3fa251ac11d1
Alee Herasimiuk <aherasimiuk@frba.utn.edu.ar>
2024-11-14 22:18:19 -0300

    Optimización reportes encuestas docente

--- src/pages/directors/TeachersSurveyDashboardPage.tsx
+++ src/pages/directors/TeachersSurveyDashboardPage.tsx
@@ -1,28 +1,11 @@
 import { useEffect, useState } from "react";
 import { Placeholder, Table } from "reactstrap";
-import Select from 'react-select';
 
 import { get } from "../../utils/network";
 import { SwalUtils } from "../../utils/SwalUtils";
 import PageWrapper from "../../components/PageWrapper";
 
 const MAX_DATE: string = (new Date(Date.now() - (new Date()).getTimezoneOffset())).toISOString().slice(0, -1).split('T')[0].toString();
-
-interface ICourse {
-  id: string;
-  courseName: string;
-}
-
-interface IAnswer {
-  id: number;
-  option: string;
-  value: string;
-}
-
-interface IAnswerData {
-  question: string;
-  answers: IAnswer[];
-}
 
 interface IQuestion {
   question1: number[],
@@ -48,45 +31,58 @@ const teacherQuestions = [
   "5. El uso de la plataforma colabora en la mejora del aprendizaje de mis alumnos.",
 ];
 
+interface ITeacherSurveyData {
+  question: string;
+  answers: {
+    option: string;
+    value: string;
+  }[];
+}
+
 export const TeachersSurveyDashboardPage = () => {
-  const [courses, setCourses] = useState<ICourse[]>([]);
-  const [courseId, setCourseId] = useState<string>('');
-  const [answers, setAnswers] = useState<IAnswerData[]>([]);
   const [dateFrom, setDateFrom] = useState<string | null>(null);
   const [dateTo, setDateTo] = useState<string | null>(null);
-  const [teachersSurveyData, setTeachersSurveyData] = useState<IQuestion | null>(null);
+  const [teachersSurveyData, setTeachersSurveyData] = useState<ITeacherSurveyData[] | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     Promise.all([
-      getCourses(),
       fetchStudentsSurveyData()
     ]).then(() => setLoading(false));
   }, []);
+
+  const mapQuestionSurveyData = (data: IQuestion) => {
+
+    const questions = Object.values(data).map((question, index) => ({
+      question: teacherQuestions[index],
+      answers: questionsOptions.map((option, index2) => ({
+        option: option,
+        value: question[index2].toString()
+      }))
+    }));
+
+    const reversed = questions.map((it) => ({
+      question: it.question,
+      answers: it.answers.reverse()
+    }))
+
+    setTeachersSurveyData(reversed);
+  }
 
   const fetchStudentsSurveyData = () => {
     get('/survey/teacher-results')
       .then(res => res.json())
       .then(res => res.data)
       .then((data: IQuestion) => {
-        setTeachersSurveyData(data);
+        mapQuestionSurveyData(data);
       });
   };
 
-  const getCourses = () => get('/directors/courses/')
-    .then(res => res.json())
-    .then(res => res.data)
-    .then((data: ICourse[]) => {
-      const allCoursesOption = { id: '', courseName: 'Todos los cursos' };
-      setCourses([allCoursesOption, ...data.map((course: ICourse) => ({
-        id: course.id,
-        courseName: course.courseName,
-      }))]);
-    });
-
   useEffect(() => {
     fetchStudentsSurveyDataFiltered();
-  }, [courseId, dateFrom, dateTo]);
+  }, [dateFrom, dateTo]);
+
+
 
   const fetchStudentsSurveyDataFiltered = async () => {
     setLoading(true);
@@ -107,10 +103,6 @@ export const TeachersSurveyDashboardPage = () => {
     let endpoint = '/survey/teacher-results';
     const queryParams: string[] = [];
 
-    if (courseId) {
-      queryParams.push(`courseId=${courseId}`);
-    }
-
     if (dateFrom && dateTo) {
       queryParams.push(`dateFrom=${dateFrom}`);
       queryParams.push(`dateTo=${dateTo}`);
@@ -127,49 +119,7 @@ export const TeachersSurveyDashboardPage = () => {
           setTeachersSurveyData(null);
           return;
         }
-        setTeachersSurveyData(data);
-        setAnswers([
-          {
-            question: teacherQuestions[0],
-            answers: questionsOptions.map((option, index) => ({
-              id: index,
-              option: questionsOptions[questionsOptions.length - 1 - index],
-              value: data.question1[questionsOptions.length - 1 - index].toString()
-            }))
-          },
-          {
-            question: teacherQuestions[1],
-            answers: questionsOptions.map((option, index) => ({
-              id: index,
-              option: questionsOptions[questionsOptions.length - 1 - index],
-              value: data.question2[questionsOptions.length - 1 - index].toString()
-            }))
-          },
-          {
-            question: teacherQuestions[2],
-            answers: questionsOptions.map((option, index) => ({
-              id: index,
-              option: questionsOptions[questionsOptions.length - 1 - index],
-              value: data.question3[questionsOptions.length - 1 - index].toString()
-            }))
-          },
-          {
-            question: teacherQuestions[3],
-            answers: questionsOptions.map((option, index) => ({
-              id: index,
-              option: questionsOptions[questionsOptions.length - 1 - index],
-              value: data.question4[questionsOptions.length - 1 - index].toString()
-            }))
-          },
-          {
-            question: teacherQuestions[4],
-            answers: questionsOptions.map((option, index) => ({
-              id: index,
-              option: questionsOptions[questionsOptions.length - 1 - index],
-              value: data.question5[questionsOptions.length - 1 - index].toString()
-            }))
-          },
-        ]);
+        mapQuestionSurveyData(data);
       }).then(() => setLoading(false));
   };
 
@@ -253,29 +203,29 @@ export const TeachersSurveyDashboardPage = () => {
               gap: '1rem',
               padding: '1rem',
             }}>
-              {answers.length > 0 && <>
-                {teachersSurveyData && answers.map((answer: IAnswerData) => (
-                  <div key={answer.question} style={{ width: '100%', border: '1px solid #ccc', padding: '1rem' }}>
-                    <h4>{answer.question}</h4>
-                    <Table striped>
-                      <thead>
-                        <tr>
-                          <th>Respuesta</th>
-                          <th>Porcentaje (%)</th>
+              
+              {teachersSurveyData?.map((it: ITeacherSurveyData, ix: number) => (
+                <div key={ix} style={{ width: '100%', border: '1px solid #ccc', padding: '1rem' }}>
+                  <h4>{it.question}</h4>
+                  <Table striped>
+                    <thead>
+                      <tr>
+                        <th>Respuesta</th>
+                        <th>Porcentaje (%)</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {it.answers.map((answer) => (
+                        <tr key={`${it.question}-${ix}`}>
+                          <th>{answer.option}</th>
+                          <th>{answer.value}</th>
                         </tr>
-                      </thead>
-                      <tbody>
-                        {answer.answers.map((answer: IAnswer) => (
-                          <tr key={answer.id}>
-                            <th>{answer.option}</th>
-                            <th>{answer.value}</th>
-                          </tr>
-                        ))}
-                      </tbody>
-                    </Table>
-                  </div>
-                ))}
-              </>}
+                      ))}
+                    </tbody>
+                  </Table>
+                </div>
+              ))}
+              
 
               {!teachersSurveyData && <>
                 <h4>Sin resultados</h4>

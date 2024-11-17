f8ded6a1a2a5ff9eb8ca7998db04586fdc582c2c
Matiassgg <matugrillo@hotmail.com>
2024-11-16 21:56:42 -0300

    go back ulr on original content

--- src/pages/sections/RawContentView.tsx
+++ src/pages/sections/RawContentView.tsx
@@ -1,13 +1,20 @@
 import { useSearchParams } from "react-router-dom";
-import { Card } from "reactstrap";
 import PageWrapper from "../../components/PageWrapper";
 
 export const RawContentView = () => {
 
   const [searchParams, setUrlSearchParams] = useSearchParams();
 
+  const getCourseFromURL = () => {
+    return window.location.pathname.split('/')[2];
+  }
+
+  const getSectionFromURL = () => {
+    return window.location.pathname.split('/')[4];
+  }
+
   return (
-    <PageWrapper title="Contenido original">
+    <PageWrapper title="Contenido original" goBackUrl={`/courses/${getCourseFromURL()}/sections/${getSectionFromURL()}`}>
       <iframe src={searchParams.get('url') as string} style={{ width: '100%', height: '100%' }}></iframe>
     </PageWrapper>
 

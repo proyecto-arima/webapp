29b3a292170a3e7f16509510c4e95f7fc73f8a43
Alee Herasimiuk <aherasimiuk@frba.utn.edu.ar>
2024-11-16 22:54:52 -0300

    Better goback button on raw content

--- src/pages/sections/RawContentView.tsx
+++ src/pages/sections/RawContentView.tsx
@@ -1,20 +1,15 @@
-import { useSearchParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import PageWrapper from "../../components/PageWrapper";
 
 export const RawContentView = () => {
 
   const [searchParams, setUrlSearchParams] = useSearchParams();
 
-  const getCourseFromURL = () => {
-    return window.location.pathname.split('/')[2];
-  }
+  const { courseId, sectionId} = useParams<{ courseId: string, sectionId: string }>();
 
-  const getSectionFromURL = () => {
-    return window.location.pathname.split('/')[4];
-  }
 
   return (
-    <PageWrapper title="Contenido original" goBackUrl={`/courses/${getCourseFromURL()}/sections/${getSectionFromURL()}`}>
+    <PageWrapper title="Contenido original" goBackUrl={`/courses/${courseId}/sections/${sectionId}`}>
       <iframe src={searchParams.get('url') as string} style={{ width: '100%', height: '100%' }}></iframe>
     </PageWrapper>
 
